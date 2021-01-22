1ff8f56eb0d57d8dfb20652fd1911a94a898cf44
Tejas Navghane <ts.navghane@gmail.com>
2021-01-22 11:59:42 +0530

    C4: Annotation & Wildcard Routes

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -5,11 +5,36 @@ declare(strict_types=1);
 namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class DefaultController
 {
+    /**
+     * @Route("/")
+     */
     public function index(): Response
     {
-        return new Response('Test Default Controller');
+        return new Response('Test Default Controller With Annotations');
+    }
+
+    /**
+     * @Route("/default/test-this-new-route")
+     */
+    public function show(): Response
+    {
+        return new Response('Test New Route With Annotations');
+    }
+
+    /**
+     * @Route("/default/{slug}")
+     */
+    public function showSlug(string $slug): Response
+    {
+        return new Response(
+            sprintf(
+                'Test New Route "%s" With Annotations',
+                ucwords(str_replace('-', ' ', $slug))
+            )
+        );
     }
 }
