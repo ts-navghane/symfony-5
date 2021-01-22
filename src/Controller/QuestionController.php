417179df54e4f39edfb0e5380f2fce35073ede51
Tejas Navghane <ts.navghane@gmail.com>
2021-01-22 18:31:47 +0530

    C10: Fundaments: Autowiring Dependencies into a Service

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -3,12 +3,10 @@
 namespace App\Controller;
 
 use App\Service\MarkdownHelper;
-use Knp\Bundle\MarkdownBundle\MarkdownParserInterface;
 use Psr\Cache\InvalidArgumentException;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Contracts\Cache\CacheInterface;
 use Twig\Environment;
 
 class QuestionController extends AbstractController
@@ -28,12 +26,8 @@ class QuestionController extends AbstractController
      * @Route("/questions/{slug}", name="app_question_show")
      * @throws InvalidArgumentException
      */
-    public function show(
-        string $slug,
-        MarkdownParserInterface $markdownParser,
-        CacheInterface $cache,
-        MarkdownHelper $markdownHelper
-    ): Response {
+    public function show(string $slug, MarkdownHelper $markdownHelper): Response
+    {
         $answers = [
             'Make sure your cat is sitting `purrrfectly` still 🤣',
             'Honestly, I like furry shoes better than MY cat',
@@ -43,8 +37,6 @@ class QuestionController extends AbstractController
         $questionText = 'I\'ve been turned into a cat, any *thoughts* on how to turn back?
         While I\'m **adorable**, I don\'t really care for cat food.';
         $parsedQuestionText = $markdownHelper->parse($questionText);
-
-        dump($cache);
 
         return $this->render(
             'question/show.html.twig',
--- src/Service/MarkdownHelper.php
+++ src/Service/MarkdownHelper.php
@@ -4,14 +4,37 @@ declare(strict_types=1);
 
 namespace App\Service;
 
+use Knp\Bundle\MarkdownBundle\MarkdownParserInterface;
+use Psr\Cache\InvalidArgumentException;
+use Symfony\Contracts\Cache\CacheInterface;
+
 class MarkdownHelper
 {
+    /**
+     * @var MarkdownParserInterface
+     */
+    private $markdownParser;
+
+    /**
+     * @var CacheInterface
+     */
+    private $cache;
+
+    public function __construct(MarkdownParserInterface $markdownParser, CacheInterface $cache)
+    {
+        $this->markdownParser = $markdownParser;
+        $this->cache = $cache;
+    }
+
+    /**
+     * @throws InvalidArgumentException
+     */
     public function parse(string $source): string
     {
-        return $cache->get(
+        return $this->cache->get(
             'markdown_'.md5($source),
-            function () use ($markdownParser, $source) {
-                return $markdownParser->transformMarkdown($source);
+            function () use ($source) {
+                return $this->markdownParser->transformMarkdown($source);
             }
         );
     }
