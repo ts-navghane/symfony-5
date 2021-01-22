7b6222c33dd9c641fcb55a31c6fabcea6f10e82f
Tejas Navghane <ts.navghane@gmail.com>
2021-01-22 14:56:53 +0530

    C13: JSON API Endpoint

---
+++ src/Controller/CommentController.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+
+class CommentController extends AbstractController
+{
+    /**
+     * @Route("/comment/{id}/vote/{direction}")
+     * @throws \Exception
+     */
+    public function commentVote(int $id, string $direction): JsonResponse
+    {
+        if ($direction === 'up') {
+            $currentVoteCount = random_int(7, 100);
+        } else {
+            $currentVoteCount = random_int(0, 5);
+        }
+
+        return $this->json(['votes' => $currentVoteCount]);
+    }
+}
