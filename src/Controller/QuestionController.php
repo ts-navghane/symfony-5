a762d13c30ace46888a34794a5e0ffddfe5554cc
Tejas Navghane <ts.navghane@gmail.com>
2021-01-22 14:27:34 +0530

    C11: Assets: CSS, Images, etc

---
+++ src/Controller/QuestionController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class QuestionController extends AbstractController
+{
+    /**
+     * @Route("/")
+     */
+    public function homepage(): Response
+    {
+        return $this->render('question/homepage.html.twig');
+    }
+
+    /**
+     * @Route("/questions/{slug}")
+     */
+    public function show($slug)
+    {
+        $answers = [
+            'Make sure your cat is sitting purrrfectly still 🤣',
+            'Honestly, I like furry shoes better than MY cat',
+            'Maybe... try saying the spell backwards?',
+        ];
+
+        return $this->render(
+            'question/show.html.twig',
+            [
+                'question' => ucwords(str_replace('-', ' ', $slug)),
+                'answers' => $answers,
+            ]
+        );
+    }
+}
